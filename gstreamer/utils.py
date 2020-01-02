8a587024a0afc6b075c64a65565632d4f0a96662
Taras Lishchenko <jackersson@meta.ua>
2020-01-02 18:20:59 +0200

    added flatten list, to_gst_string

--- gstreamer/utils.py
+++ gstreamer/utils.py
@@ -1,3 +1,4 @@
+import typing as typ
 import numpy as np
 
 import gi
@@ -33,3 +34,26 @@ def gst_buffer_with_pad_to_ndarray(buffer: Gst.Buffer, pad: Gst.Pad, channels: i
 def numpy_to_gst_buffer(array: np.ndarray) -> Gst.Buffer:
     """Converts numpy array to Gst.Buffer"""
     return Gst.Buffer.new_wrapped(array.tobytes())
+
+
+def flatten_list(in_list: typ.List) -> typ.List:
+    """Flattens list"""
+    result = []
+    for item in in_list:
+        if isinstance(item, list):
+            result.extend(flatten_list(item))
+        else:
+            result.append(item)
+    return result
+
+
+def to_gst_string(plugins: typ.List[str]) -> str:
+    """ Generates string representation from list of plugins """
+
+    if len(plugins) < 2:
+        return ""
+
+    plugins_ = flatten_list(plugins)
+
+    # <!> between plugins (except tee)
+    return plugins_[0] + "".join([f"{'' if pl[-1] == '.' else ' !'} {pl}" for pl in plugins_[1:]])
