151efb270a727f7b7963fe838d068209aa1685c2
Lucas Koelman <lucas@westray.eu>
2021-05-07 13:54:06 +0200

    Fix python 3.8 compatibility

--- gstreamer/3rd_party/build.sh
+++ gstreamer/3rd_party/build.sh
@@ -5,6 +5,11 @@ echo "PWD: $PWD"
 # Gstreamer
 #cd gstreamer
 #./build.sh
-meson build
+BUILD_OPTS=""
+if [[ -n "$GST_PREFIX" ]]; then
+    BUILD_OPTS="${BUILD_OPTS} --prefix ${GST_PREFIX} --libdir=${GST_PREFIX}/lib"
+fi
+echo $BUILD_OPTS
+meson setup --wipe $BUILD_OPTS build
 #ninja -C build
 ninja -C build install
